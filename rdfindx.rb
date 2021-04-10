42331bf91f64907cb2e67c17019bb663f42edaf5
yunie.yx <yunie.yx@alibaba-inc.com>
2021-04-10 19:09:14 +0800

    update

--- rdfindx.rb
+++ rdfindx.rb
@@ -6,11 +6,10 @@ class Rdfindx < Formula
   revision 2
 
   bottle do
-    cellar :any
-    sha256 "4531928ed8d78b25d6a8d403ca81d8c2b8ddc59a884ab45dfa16d0c0be553c8d" => :sierra
-    sha256 "37fd65e7eb7f130794dcf334e3f765af8213d711889a854d67dfa01950343bad" => :el_capitan
-    sha256 "369d6dd1f73c0af431ad7a5babe40ea08cb9906f56d6dde20e16e72f9f2d5b14" => :yosemite
-    sha256 "57fe67783383110ec6c954fc605541ae7095ea1e39a4ef5c75a00d9e95395a51" => :mavericks
+    sha256 cellar: :any, sierra:     "4531928ed8d78b25d6a8d403ca81d8c2b8ddc59a884ab45dfa16d0c0be553c8d"
+    sha256 cellar: :any, el_capitan: "37fd65e7eb7f130794dcf334e3f765af8213d711889a854d67dfa01950343bad"
+    sha256 cellar: :any, yosemite:   "369d6dd1f73c0af431ad7a5babe40ea08cb9906f56d6dde20e16e72f9f2d5b14"
+    sha256 cellar: :any, mavericks:  "57fe67783383110ec6c954fc605541ae7095ea1e39a4ef5c75a00d9e95395a51"
   end
 
   depends_on "nettle"
@@ -31,4 +30,3 @@ class Rdfindx < Formula
     assert !File.exist?("folder/file2")
   end
 end
-
